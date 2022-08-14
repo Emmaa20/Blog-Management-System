fb0249c5db547cfd1b00a85a337dd29e4491ea88
Emmaa20 <elmirafk@code.edu.az>
2022-08-14 17:57:52 +0400

    Added properties and constrtuctor to the User class

--- Blog_Management_System/Blog_Management_System/UI/Program.cs
+++ Blog_Management_System/Blog_Management_System/UI/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Blog_Management_System.ApplicationLogic;
+using Blog_Management_System.ApplicationLogic.Services;
+using System;
 
 namespace Blog_Management_System.UI
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+       
+         
+            public static void Main(string[] args)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine();
+                Console.WriteLine("                             *******************************************************                ");
+                Console.WriteLine("                             *********** WELCOME TO OUR APPLICATION *******************                ");
+                Console.WriteLine("                             *******************************************************                ");
+                Console.Beep();
+                Console.WriteLine();
+                Console.WriteLine("/register");
+                Console.WriteLine("/login");
+                Console.WriteLine("/show-blogs-with-comments");
+                Console.WriteLine("/show-filtered-blogs-with-comments");
+                Console.WriteLine("/find-blog-by-code");
+                Console.WriteLine("/exit");
+                Console.WriteLine();
+                while (true)
+                {
+                    Console.Write("Enter suitable command :");
+                    string command = Console.ReadLine();
+                    if (command == "/register")
+                    {
+                        Authentication.Register();
+                    }
+                    else if (command == "/login")
+                    {
+                        Authentication.Login();
+                    }
+                    else if (command == "/show-blogs-with-comments")
+                    {
+                        BlogService.ShowBlogsWithComment();
+                    }
+                    else if (command == "/show-filtered-blogs-with-comments")
+                    {
+                        BlogService.ShowFilteredBlogsWithComments();
+                    }
+                    else if (command == "/find-blog-by-code")
+                    {
+                        BlogService.FindBlogByCode();
+                    }
+                    else if (command == "/exit")
+                    {
+                        Console.WriteLine("Thanks for using our application");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Command not found ...");
+                    }
+                }
+            }
         }
+
+      
     }
-}
+
+    
+
