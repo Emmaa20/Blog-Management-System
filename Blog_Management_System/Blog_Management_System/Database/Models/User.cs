21dfae7271cb4412de2b1685bbb3bf18b92b790d
Emmaa20 <elmirafk@code.edu.az>
2022-08-14 20:21:33 +0400

    Added properties and constructor to the User class

--- Blog_Management_System/Blog_Management_System/Database/Models/User.cs
+++ Blog_Management_System/Blog_Management_System/Database/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog_Management_System.Database.Models.Common;
+using Blog_Management_System.Database.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,27 @@ using System.Threading.Tasks;
 
 namespace Blog_Management_System.Database.Models
 {
-    internal class User
+    public class User : Entity<int>  //The user model is ready
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        protected DateTime Time { get; set; } = DateTime.Now;
+
+        public User(string name, string surname, string email, string password)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            Id = UserRepository.IDCounter;
+        }
+
+        public virtual string GetInfo()
+        {
+            return Name + " " + Surname;
+        }
+
     }
 }
